d56d9159a93ba1f9c9378ee1c8c24459d244f68b
lucas <lucas.johannson1@ucalgary.ca>
2021-06-03 21:21:01 +0000

    cat breed api

---
+++ test/breedFetcherTest.js
@@ -0,0 +1,36 @@
+// breedFetcherTest.js
+
+const { fetchBreedDescription } = require('../breedFetcher');
+const { assert } = require('chai');
+
+describe('fetchBreedDescription', () => {
+  it('returns a string description for a valid breed, via callback', (done) => {
+    fetchBreedDescription('Siberian', (err, desc) => {
+      // we expect no error for this scenario
+      assert.equal(err, null);
+
+      const expectedDesc = "The Siberians dog like temperament and affection makes the ideal lap cat and will live quite happily indoors. Very agile and powerful, the Siberian cat can easily leap and reach high places, including the tops of refrigerators and even doors.";
+      console.log("testing", desc);
+
+      // compare returned description
+      assert.equal(expectedDesc, desc.trim());
+      
+      done();
+    });
+  });
+
+  it('returns a string description for a valid breed, via callback', (done) => {
+    fetchBreedDescription('lol', (err, desc) => {
+      // we expect no error for this scenario
+      assert.equal(err, "breed not found");
+
+      const expectedDesc = null;
+      //console.log("testing", desc);
+
+      // compare returned description
+      assert.equal(expectedDesc, desc);
+      
+      done();
+    });
+  });
+});
